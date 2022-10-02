1b4e58900e038730158f8e5a05804b847139de31
Ketan Vishwakarma <ketanv288@gmail.com>
2022-10-02 13:47:28 +0530

    Added Solution for DCP#216.py

--- DCP#216.py
+++ DCP#216.py
@@ -29,26 +29,32 @@ print(choices)
 choice_list =[]
 value_list = []
 
-def checkForCharacters(x:list):
-    # for choices in choice_list:
-    #     for secondChoice in choice_list:
-    #         if choices == "I" and secondChoice == "V":
-    #             print(Roman_dic[str(secondChoice)] - Roman_dic[str(choice)])
-    print(choice_list[-2])
-    if choice_list[-2] == "I" and choice_list[-1] == "V":
-        print(sum(value_list)-1)
-
+def checkForCharacters(x):
+    addedValue = sum(value_list)
+    if len(value_list) >= 2:
+        if str(choice_list[-2]) == "I" and str(choice_list[-1]) == "V":
+            print("The sum of Roman Numbers: " + str(int(addedValue-2)))
+        elif str(choice_list[-2]) == "I" and str(choice_list[-1]) == "X":
+            print("The sum of Roman Numbers: " + str(int(addedValue -2)))
+        elif str(choice_list[-2]) == "X" and str(choice_list[-1]) == "L":
+            print("The sum of Roman Numbers: " + str(int(addedValue -20)))
+        elif str(choice_list[-2]) == "L" and str(choice_list[-1]) == "C":
+            print("The sum of Roman Numbers: " + str(int(addedValue -60)))
+        elif str(choice_list[-2]) == "C" and str(choice_list[-1]) == "D":
+            print("The sum of Roman Numbers: " + str(int(addedValue -200)))
+        elif str(choice_list[-2]) == "C" and str(choice_list[-1]) == "M":
+            print("The sum of Roman Numbers: " + str(int(addedValue -200)))
+        else:
+            print("The sum of Roman Numbers: " + str(addedValue))
+    else:
+        print("The sum of Roman Numbers: " + str(addedValue))
 
 for choice in choices:
     if len(choices) >= 1:
         if str(choice) in Roman_dic:
-            # print(Roman_dic[str(choice)])
             value_list.append(int(Roman_dic[str(choice)]))
             choice_list.append(choice)
         else:
             print("Please pass valid Roman Number")
 
 checkForCharacters(choice_list)
-
-# print()
-print(choice_list)
